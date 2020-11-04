8c98f7b5afe67b8ee278074609c908240ff7fe2b
DESKTOP-HRRNGKA\Administrator <1138894663@qq.com>
2020-11-04 18:29:00 +0800

    350 70

--- src/动态规划/_70_爬楼梯.java
+++ src/动态规划/_70_爬楼梯.java
@@ -7,9 +7,23 @@ package 动态规划;
  */
 public class _70_爬楼梯 {
 
-    //
+    // 解法1:递归法
     public int climbStairs1(int n) {
         if (n <= 2) return n;
         return climbStairs1(n - 1) + climbStairs1(n - 2);
     }
+
+    // 解法2:动态规划法
+    public int climbStairs2(int n) {
+        if (n <= 2) return n;
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            second = first + second;
+            first = second - first;
+        }
+        return second;
+    }
+
+
 }
--- src/数组/_350_两个数组的交集II.java
+++ src/数组/_350_两个数组的交集II.java
@@ -1,11 +1,80 @@
 package 数组;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * @Author zhoushaodong
  * @Description https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
  * @Date 2020/10/30 16:13
  */
 public class _350_两个数组的交集II {
+    /**
+     * 解法1:哈希表
+     * 1.将数组较大的遍历放进哈希表，记录数组出现的次数
+     * 2.遍历较小的数组，如果哈希表中的次数大于0，则减1，写会哈希表，并把值添加到返回的数组中。
+     */
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return inner_intersect(nums1, nums2);
+        } else {
+            return inner_intersect(nums2, nums1);
+        }
+    }
+
+    public int[] inner_intersect(int[] maxArr, int[] minArr) {
+        HashMap<Integer, Integer> map = new HashMap<>(maxArr.length);
+
+        int[] resultArr = new int[maxArr.length];
+        int index = 0;
+        for (int num : maxArr) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        for (int num : minArr) {
+            if (map.containsKey(num)) {
+                int oldcount = map.get(num);
+                if (oldcount > 0) {
+                    map.put(num, oldcount - 1);
+                    resultArr[index++] = num;
+                }
+            }
+        }
+        return Arrays.copyOf(resultArr, index);
+    }
+
+    /**
+     * 解法2:排序 + 双指针
+     * 1.先对2个数组进行排序
+     * 2.定义2个指针分别指向2个数组的开头
+     * 3.每次比较两个指针指向的两个数组中的数字，如果两个数字不相等，则将指向较小数字的指针右移一位，如果两个数字相等，将该数字添加到答案，并将两个指针都右移一位
+     * 4.当至少有一个指针超出数组范围时，遍历结束。
+     */
+    public int[] intersect2(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int index1 = 0, index2 = 0, index = 0;
+        int[] resultArr = new int[Math.min(length1, length2)];
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                resultArr[index++] = nums1[index1];
+                index1++;
+                index2++;
+            }
+        }
+        return Arrays.copyOf(resultArr,index);
+    }
+
     public static void main(String[] args) {
 
     }
